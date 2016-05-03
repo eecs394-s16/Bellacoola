1b8fca273ea95f3aa4ec9e83f1533e6e2d5abfc8
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-05-03 08:53:06 -0500

    Pi demo web app

---
+++ pi/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.bell').on('click', function() {
+        var settings = new Firebase('https://bellacoola.firebaseio.com/');
+        settings.child('testsettings').on('value', function(snapshot) {
+            userSetting = snapshot.val();
+            if (userSetting.silence) {
+                $.ajax({
+                    method: 'GET',
+                    url: 'http://localhost:5000/ring',
+                });
+            } else {
+                var audio = new Audio('bell.mp3');
+                audio.play();
+            }
+        });
+    });
+});
