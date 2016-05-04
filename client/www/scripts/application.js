b1edec7243da9608f080d0cd82f1eb134e09ac0f
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-05-04 01:11:05 -0500

    started working on making changes to new silence view

--- client/www/scripts/application.js
+++ client/www/scripts/application.js
@@ -3,18 +3,59 @@ angular.module('SteroidsApplication', [
     'firebase'
 ])
 .controller('IndexController', function($scope, supersonic) {
-    $scope.navbarTitle = "Home"
+    $scope.navbarTitle = "Home";
+    $scope.test = function() {
+        supersonic.logger.log('test');
+    };
 })
-
-
 .controller('SilenceController', function($scope, supersonic) {
-    $scope.navbarTitle = "Silence Settings"
+    $scope.navbarTitle = "Silence Settings";
+    $scope.test = function() {
+        supersonic.logger.log('test');
+    };
+
     $scope.update = function() {
         supersonic.logger.log('test');
         supersonic.logger.log($scope.data.silence);
-        var settingsRef = new Firebase('https://bellacoola.firebaseio.com/testsettings');
-        settingsRef.set({
-            'silence': $scope.data.silence
+        supersonic.logger.log($scope.data.duration);
+        supersonic.logger.log('test2');
+        var currentTime = Date.now();
+        var expireTime = Date.now();
+        suppersonic.logger.log(currentTime);
+        suppersonic.logger.log('testmore');
+        switch ($scope.data.duration) {
+            case "30 Minutes":
+                expireTime = addMinutes(expireTime, 30);
+                break;
+            case "1 Hour":
+                expireTime = addMinutes(expireTime, 60);
+                break;
+            case "2 Hours":
+                expireTime = addMinutes(expireTime, 120);
+                break;
+            case "3 Hours":
+                expireTime = addMinutes(expireTime, 180);
+                break;
+        };
+        var piRef = new Firebase('https://bellacoola.firebaseio.com/pi/');
+        piRef.set({
+            '1': {
+                'expiration_time': expireTime.toString(),
+                'contacts': ['+3126191065']
+                // TODO: Add contact numbers here
+            }
+        }, function() {
+            var options = {
+                message: "Your silence settings has been updated!",
+                buttonLabel: "Ok"
+            };
+            supersonic.ui.dialog.alert("Update", options).then(function() {
+                supersonic.logger.log("Alert closed.");
+            });
         });
+    }
+
+    var addMinutes = function(date, minutes) {
+        return new Date(date.getTime() + minutes*60000);
     }
 });
