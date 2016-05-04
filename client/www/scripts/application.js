a2bb61b889890f58719f51f5df18e9da238a1d75
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-05-03 22:46:33 -0500

    add silence settings page

--- client/www/scripts/application.js
+++ client/www/scripts/application.js
@@ -1,23 +1,20 @@
 angular.module('SteroidsApplication', [
-  'supersonic',
-  'firebase'
+    'supersonic',
+    'firebase'
 ])
 .controller('IndexController', function($scope, supersonic) {
-
-  $scope.navbarTitle = "Bellacoola"
-
-  $scope.update = function() {
-      supersonic.logger.log('test');
-      supersonic.logger.log($scope.data.silence);
-
-      //if ($scope.data.silence) {
-          var settingsRef = new Firebase('https://bellacoola.firebaseio.com/testsettings');
-          settingsRef.set({
-              'silence': $scope.data.silence
-          });
-      //}
-  }
+    $scope.navbarTitle = "Home"
+})
 
 
-
+.controller('SilenceController', function($scope, supersonic) {
+    $scope.navbarTitle = "Silence Settings"
+    $scope.update = function() {
+        supersonic.logger.log('test');
+        supersonic.logger.log($scope.data.silence);
+        var settingsRef = new Firebase('https://bellacoola.firebaseio.com/testsettings');
+        settingsRef.set({
+            'silence': $scope.data.silence
+        });
+    }
 });
