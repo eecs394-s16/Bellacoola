585e1982dbe1c7b4f7b79901b84b5d88f7bf3b25
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-05-17 16:45:42 -0500

    Page for buzztone setings added on mobile client

--- client/www/scripts/application.js
+++ client/www/scripts/application.js
@@ -22,6 +22,27 @@ angular.module('SteroidsApplication', [
     }
 })
 
+
+.controller('RingtoneController', function($scope, supersonic) {
+    $scope.navbarTitle = "Ringtone Settings";
+    $scope.updateRingtone = function() {
+        supersonic.logger.log('update ringtone called!');
+        var ringtoneRef = new Firebase('https://bellacoola.firebaseio.com/ringtone/');
+
+        ringtoneRef.set({
+            'ringtone': $scope.ringtone.replace(' ', '_'),
+        }, function() {
+            var options = {
+                message: "Ringtone successfully updated!",
+                buttonLabel: "Ok"
+            };
+            supersonic.ui.dialog.alert("Update", options).then(function() {
+                supersonic.logger.log("Alert closed.");
+            });
+        });
+    }
+})
+
 .controller('SilenceController', function($scope, supersonic) {
     $scope.navbarTitle = "Silence Settings";
 
